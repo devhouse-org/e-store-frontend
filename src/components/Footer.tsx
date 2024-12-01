64ff8f02d05d621f2ae18c7b7030f9630492fa08
mohammedJameel <mohammed.jameel@iqdevhouse.com>
2024-12-01 14:30:55 +0300

    footer and navbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import { Home, Mail, Paperclip, Pin, Recycle, Shield, UsersRound } from 'lucide-react'
 import React from 'react'
 import location from "../assets/images/location.png"
+import cash from "../assets/images/cash.png"
 
 type Props = {}
 
@@ -10,9 +11,9 @@ const Footer = (props: Props) => {
             <div className='px-4 py-4'>
                 <h1 className='font-tajawal-medium my-4 text-orange-400 text-2xl'>متواجدون دائماً لمساعدتكم</h1>
                 <div className="top flex justify-between ">
-                    <div className='flex gap-x-12 '>
+                    <div className='flex gap-x-12'>
 
-                        <div className="">
+                        <div className="flex-1">
                             <div className=''>
                                 <h1 className='font-tajawal-medium text-white border-b border-b-light-800'>تواصل معنا</h1>
                                 <div className='flex gap-x-4 py-4'>
@@ -40,15 +41,20 @@ const Footer = (props: Props) => {
                             </div>
                         </div>
 
-                        <div className="">
+                        <div className="flex-1">
                             <div className=''>
                                 <h1 className='font-tajawal-medium text-white border-b border-b-light-800'>طرق الدفع</h1>
                                 <div className='flex gap-x-4 py-4'>
-                                    <Home className='text-white' />
-                                    <div className='gap-x-4 items-center'>
-                                        <p className='font-tajawal-bold text-orange-400'>واتساب</p>
-                                        <p className='text-white'>9647712345643+</p>
-                                        <p className='text-white'>9647812345643+</p>
+                                    <div className='gap-y-4 flex flex-col items-center'>
+                                        <div className='bg-black flex justify-center p-4 rounded-md'>
+                                            <img className='w-[80px]' src="https://zaincash.iq/assets/images/logo.png?v=0.0.2.5" alt="" />
+                                        </div>
+                                        <div className='bg-white flex justify-center p-4 rounded-md'>
+                                            <img className='w-[80px]' src="https://taifpay.iq/images/logo2.png" alt="" />
+                                        </div>
+                                        <div className='bg-black flex justify-center p-4 rounded-md'>
+                                            <img className='w-[80px]' src={cash} alt="" />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
