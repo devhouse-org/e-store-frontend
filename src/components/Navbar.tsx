9bba2a96c4bcf80e65aaf7dcc9cffbea942a59fe
mohammedJameel <mohammed.jameel@iqdevhouse.com>
2024-12-01 10:07:08 +0300

    navbar and tailwind config

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 const Navbar = (props: Props) => {
   return (
-    <div className="bg-green-500">
+    <div className="bg-green-100 rounded-lg overflow-hidden">
       {props.hasAd && (
         <div className="ad bg-orange-500 text-white font-bold flex justify-center items-center py-1">
           <h1>{props.adTitle}</h1>
@@ -14,19 +14,19 @@ const Navbar = (props: Props) => {
 
       <div
         dir="rtl"
-        className="navigation flex items-center py-1 px-2 justify-between"
+        className="navigation flex items-center py-2 px-2 justify-between"
       >
         <ul className="flex gap-x-4 list-none flex-1">
-          <li className="px-1 py-1 hover:bg-gray-500/50">Home</li>
-          <li className="px-1 py-1 hover:bg-gray-500/50">Contact</li>
-          <li className="px-1 py-1 hover:bg-gray-500/50">alskdj</li>
-          <li className="px-1 py-1 hover:bg-gray-500/50">asdlifkj</li>
-          <li className="px-1 py-1 hover:bg-gray-500/50">asldkfj</li>
+          <li className="px-1 hover:bg-gray-500/50 font-tajawal-bold">الرئيسية</li>
+          <li className="px-1 hover:bg-gray-500/50">Contact</li>
+          <li className="px-1 hover:bg-gray-500/50">alskdj</li>
+          <li className="px-1 hover:bg-gray-500/50">asdlifkj</li>
+          <li className="px-1 hover:bg-gray-500/50">asldkfj</li>
         </ul>
-        <div className="logo flex-1 bg-red-500 flex justify-center items-center">
+        <div className="logo flex-1 flex justify-center items-center">
           Logo
         </div>
-        <div dir="ltr" className="icons flex-1 bg-blue-500">
+        <div dir="ltr" className="icons flex-1">
           Icons
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,64 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: {
+          50: "#FFF3ED",
+          100: "#FFE1D5",
+          200: "#FFC1AA",
+          300: "#FF9F7F",
+          400: "#FF7D54",
+          500: "#EF5D2A",
+          600: "#CC4D23",
+          700: "#A33D1C",
+          800: "#7A2D15",
+          900: "#521D0E",
+        },
+        light: {
+          50: "#FFFFFF",
+          100: "#FCFCFC",
+          200: "#FAFAFA",
+          300: "#F7F7F7",
+          400: "#F5F5F5",
+          500: "#F4F4F4",
+          600: "#E0E0E0",
+          700: "#CCCCCC",
+          800: "#B8B8B8",
+          900: "#A4A4A4",
+        },
+        dark: {
+          50: "#F2F2F2",
+          100: "#DADADA",
+          200: "#B3B3B3",
+          300: "#8B8B8B",
+          400: "#646464",
+          500: "#1A202C",
+          600: "#171C25",
+          700: "#13171E",
+          800: "#0F1217",
+          900: "#0B0D10",
+        },
+        blue: {
+          50: "#E5F2F8",
+          100: "#CCE5F0",
+          200: "#99CCE0",
+          300: "#66B3D1",
+          400: "#339AC1",
+          500: "#0A2F4B",
+          600: "#08273E",
+          700: "#061F31",
+          800: "#041725",
+          900: "#020F18",
+        },
+      },
+      fontFamily: {
+        "tajawal-light": ["TajawalLight"],
+        "tajawal-regular": ["TajawalRegular"],
+        "tajawal-medium": ["TajawalMedium"],
+        "tajawal-bold": ["TajawalBold"]
+      }
+    },
   },
   plugins: [],
 };
