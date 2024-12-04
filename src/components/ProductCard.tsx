6fd987e43965360e26584909f546e796e58fe9f5
mohammedJameel <mohammed.jameel@iqdevhouse.com>
2024-12-04 16:10:04 +0300

    .

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -17,40 +17,35 @@ const ProductCard: React.FC<ProductCardProps> = ({
 }) => {
   return (
     <div
-      className={`${size === "lg" ? "w-64 p-4" : "w-40 p-2"} ${
-        activeCard ? "border-orange-400" : "border-transparent"
-      } border bg-white shadow-md rounded-lg overflow-hidden text-center`}
+      className={`${size === "lg" ? "w-64 p-4" : "w-40 p-2"} ${activeCard ? "border-orange-400" : "border-transparent"
+        } border bg-white shadow-md rounded-lg overflow-hidden text-center`}
     >
       {/* Product Image */}
       <div
-        className={`${
-          size === "lg" ? "p-6" : "p-4"
-        } flex justify-center h-24 items-center`}
+        className={`${size === "lg" ? "p-6" : "p-4"
+          } flex justify-center items-center`}
       >
         <img
           src={productImage}
           alt={productName}
-          className={`${
-            size === "lg" ? "w-full" : "w-24"
-          } object-cover rounded-md`}
+          className={`${size === "lg" ? "w-full h-26" : "h-24 w-24"
+            } object-contain rounded-md `}
         />
       </div>
 
       {/* Product Name */}
       <div className={`p-${size === "lg" ? "4" : "2"}`}>
         <h2
-          className={`${
-            size === "lg" ? "text-lg" : "text-sm"
-          } text-gray-800 font-tajawal-medium truncate`}
+          className={`${size === "lg" ? "text-lg" : "text-sm"
+            } text-gray-800 font-tajawal-medium truncate`}
         >
           {productName}
         </h2>
 
         {/* Product Price */}
         <p
-          className={`${
-            size === "lg" ? "text-xl" : "text-sm"
-          } text-orange-500 font-tajawal-bold truncate`}
+          className={`${size === "lg" ? "text-xl" : "text-sm"
+            } text-orange-500 font-tajawal-bold truncate`}
         >
           {productPrice.toLocaleString()} د.ع
         </p>
