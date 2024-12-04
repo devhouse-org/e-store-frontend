103ff819f7c49517b9d5b61395a7d384739d4d99
mohammedJameel <mohammed.jameel@iqdevhouse.com>
2024-12-04 12:04:10 +0300

    stepper

--- src/pages/Components.tsx
+++ src/pages/Components.tsx
@@ -23,8 +23,13 @@ import SpecialProducts from "@/components/SpecialProducts";
 import AuctionSection from "@/components/AuctionSection";
 import Filter from "@/components/Filter";
 import LocationCard from "@/components/LocationCard";
+import Stepper from "@/components/Stepper";
+
+const tabs = ["سلة التسوق", "الشحن والتسليم", "الدفع"]
 
 const Components = () => {
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+
   const handleSubscribe = () => {
     alert("Subscribed!");
   };
@@ -64,6 +69,13 @@ const Components = () => {
       <div className="special-products">
         <SectionTitle title="Special Products" />
         <SpecialProducts />
+      </div>
+
+      <div className="cards">
+        <SectionTitle title="Stepper" />
+        <div>
+          <Stepper tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} />
+        </div>
       </div>
 
       <div className="cards">
@@ -217,8 +229,8 @@ const Components = () => {
             <ProductsTable
               // products={useProductStore}
               total={total}
-              // onQuantityChange={handleQuantityChange}
-              // onRemove={handleRemove}
+            // onQuantityChange={handleQuantityChange}
+            // onRemove={handleRemove}
             />
           </div>
           {/* Auction Section */}
