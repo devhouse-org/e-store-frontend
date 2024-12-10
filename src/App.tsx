fe3a65ef0ce15bacc58d19a8b76ac7600dc9bc65
Yaser <yaserasaad500@gmail.com>
2024-12-10 12:51:56 +0300

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import { Route, Routes } from "react-router-dom";
-// import "./App.css";
+import Layout from "./components/Layout";
 import Components from "./pages/Components";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div dir="rtl">
       <Routes>
-        <Route path="/" element={<Components />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="components" element={<Components />} />
+        </Route>
       </Routes>
     </div>
   );
