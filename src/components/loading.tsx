9806660705da3f4502f376813e4b06bc07d813af
Hussein Najah <husseinnajah123@gmail.com>
2025-03-12 10:39:06 +0300

    Update loading.tsx

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,7 +1,7 @@
 const LoadingComponent = () => {
   return (
     <div className="rounded-xl size-full flex flex-col items-center justify-center gap-2 p-5 bg-gray-100">
-      <div className="animate-spin w-10 h-10 border-t-2 border-b-2 border-gray-900 rounded-full"></div>
+      <div className="animate-spin border-primary w-10 h-10 border-t-2 border-b-2 rounded-full"></div>
       <p className="text-sm text-gray-500">جاري التحميل</p>
     </div>
   );
