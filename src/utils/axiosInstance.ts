472b8caef028e8d838ad70197e86ac623f56769d
Ahmed Ghazi <ahmed.ghazi@iqdevhouse.com>
2025-03-13 20:39:31 +0300

    Update axios instance to use environment variable for base URL

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   // timeout: 10000, // 10 seconds timeout
   // headers: {
   //   'Content-Type': 'application/json',
