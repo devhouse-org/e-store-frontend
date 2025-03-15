709e97668c0bcaee92acebc7dcc3caad52e52ce3
Hussein Najah <husseinnajah123@gmail.com>
2025-03-15 21:54:51 +0300

    Update WishlistEmpty component with new design and functionality. Replace static text with dynamic elements, add HeartCrack icon, and implement a button for shopping. Enhance layout for better user experience when wishlist is empty.

--- src/components/wishlist/WishlistEmpty.tsx
+++ src/components/wishlist/WishlistEmpty.tsx
@@ -1,17 +1,27 @@
+import { HeartCrack } from "lucide-react";
 import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
 
 const WishlistEmpty = () => {
   return (
-    <div className="container px-4 py-20 mx-auto text-center">
-      <h1 className="font-tajawal-bold mb-4 text-3xl text-gray-800">
-        قائمة المفضلة
-      </h1>
-      <p className="mb-8 text-gray-600">لا توجد منتجات في المفضلة.</p>
-      <Link
-        to="/products"
-        className="hover:bg-orange-600 inline-block px-6 py-3 text-white bg-orange-500 rounded-md"
-      >
-        تصفح المنتجات
+    <div className="min-h-[80vh] flex flex-col items-center justify-center gap-8 px-4 max-w-md mx-auto">
+      <div className="space-y-4 text-center">
+        <div className="w-fit animate-pulse p-4 mx-auto bg-orange-100 rounded-full">
+          <HeartCrack className="w-10 h-10 text-orange-500" />
+        </div>
+        <h2 className="font-tajawal-bold text-3xl text-gray-800">
+          المفضلة فارغة
+        </h2>
+        <p className="font-tajawal-regular text-lg text-gray-600">
+          لم تقم بإضافة أي منتج إلى المفضلة بعد
+        </p>
+      </div>
+
+      <Link to="/products">
+        <Button
+          label="تسوق الآن"
+          className="hover:bg-orange-600 rounded-xl hover:shadow-orange-200 px-8 py-6 text-lg text-white transition-all duration-300 bg-orange-500 shadow-lg"
+        />
       </Link>
     </div>
   );
